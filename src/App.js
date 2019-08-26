9a200c696968c566efa190bf9b703dd738f495ff
Jay Fallon <jayfallon@gmail.com>
2019-08-26 09:44:59 -0400

    finished workflow -- not working with webpack 3 syntax

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import { Route, Link } from 'react-router-dom';
+
+import asyncComponent from './hoc/asyncComponent';
+import Users from './containers/Users';
+
+const AsyncPizza = asyncComponent(() => import('./containers/Pizza'));
+
+class App extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <div>
+          <Link to="/">Users</Link> | <Link to="/pizza">Pizza</Link>
+        </div>
+        <div>
+          <Route path="/" exact component={Users} />
+          <Route path="/pizza" exact component={AsyncPizza} />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/containers/Users.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class Users extends Component {
+  state = {};
+
+  render() {
+    return (
+      <div>
+        <h1>The Users</h1>
+        <p>My users</p>
+      </div>
+    );
+  }
+}
+
+export default Users;
